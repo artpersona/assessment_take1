292674fb8e993c8f908c3264ee3dad7aec5ad612
John Paul Perez <jp11perez@gmail.com>
2022-02-18 11:57:16 +0800

    commit with error

--- src/route/index.js
+++ src/route/index.js
@@ -18,36 +18,7 @@ function index() {
 const MainNavigation = () => {
   const { loggedUser } = useAuthContext();
 
-  return (
-    <Routes>
-      <Route
-        exact
-        path="/"
-        element={
-          !loggedUser ? (
-            <Navigate to="/home" />
-          ) : (
-            (loggedUser.role = "admin" ? (
-              <Navigate to="/dashboard" />
-            ) : (
-              <Navigate to="/home" />
-            ))
-          )
-        }
-      />
-      <Route path="/home" element={<Landing />} />
-      {/* {/* <Route path="/services" element={<Services />} /> */}
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/tech-stack" element={<TechStack />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/form" element={<FormPage />} />
-      <Route path="/applicants" element={<Applicants />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+  return <div>YAWAAA</div>;
 };
 
 const PrivateRoute = ({ children, redirectTo }) => {
